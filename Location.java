67ef80a37d46ef5deb1c7a3a21f3153a62e0e00e
Rachel <rkunkel@smith.edu>
2024-04-16 13:21:37 -0400

    working on location.java (created a WIP constructor for a location)

--- Location.java
+++ Location.java
@@ -1,6 +1,19 @@
 public class Location {
+    public String direction;
+
+    // Constructor for a location 
+    public Location(String direction) {
+        this.direction = direction;
+    }
+
     // Print/Create information about location
     // Exploring / Looking around
     // Create NPCs associated w/ location 
 
+    public static void main(String[] args) {
+        Location Kingdom = new Location("West");
+        Location Village = new Location("East");
+        Location Forest = new Location("North");
+        Location Ocean = new Location("South");
+    }
 }
