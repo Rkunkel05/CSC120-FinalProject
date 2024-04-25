6ba03c4d381102f0ad3b89f73c93129b91f0accc
27hsarhan <158182999+27hsarhan@users.noreply.github.com>
2024-04-25 10:53:28 -0400

    Create Dialogue.java

---
+++ Dialogue.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class Dialogue {
+
+  public static void main(String[] arguments) {
+    ArrayList<String> transcript = new ArrayList<String>();
+   // System.out.print(file:welcomeMessage);
+    // transcript.add("How many rounds?");
+    Scanner input = new Scanner(System.in);
+    int nRounds = input.nextInt();
+    input.nextLine(); //clearing new line character
+    transcript.add(String.valueOf(nRounds));
+    System.out.println("Hi there! What would you like to do next?"); 
+    //transcript.add("Hi there! What would you like to do next?");
+    for (int i = 0; i < nRounds; i++){
+       String user_input = input.nextLine();
+     // transcript.add(user_input);
+      System.out.println(dialogue);
+
+//if user input is north, print forestMessage
+//select either 
+      
+      //String strArray[];
+        //String inputArray = new strArray[];
+      // pass lines through a new array, print the arry
+      //System.out.println("Transcript:" + )
+      //if(userInput contains i):
+        //print(f{"you" + input})
+      //else input contains am:
+        //print(f{"are" + input})
+    }
+    System.out.println("Goodbye.");
+  }
+}
