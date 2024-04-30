11d397c07bd56ce895f703c73caef00ed8431aae
27hsarhan <158182999+27hsarhan@users.noreply.github.com>
2024-04-30 00:07:53 -0400

    Add files via upload

---
+++ NPC.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class NPC {
+
+  Scanner conversation = new Scanner(System.in);
+  String userInput;
+  String name;
+
+  public static String talkToUser(){
+    System.out.println("Hi! My name is" + name ". What's yours?");
+    if (userInput == )
+  }
+  public static String tradeObject(){
+    
+  }
+  
+  public static void main(String[] args){
+  
+  }
+  
+}
