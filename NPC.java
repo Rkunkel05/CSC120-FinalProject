bafe2c90ee87d1f1b0f65bea537c0b7cd5f553d0
Rachel <rkunkel@smith.edu>
2024-05-02 00:02:57 -0400

    commit

--- NPC.java
+++ NPC.java
@@ -6,9 +6,6 @@ import java.util.Hashtable;
 
 class NPC {
 
-    int skill;
-    int armour;
-
     Scanner conversation = new Scanner(System.in);
     String userInput;
     String name;
@@ -35,23 +32,23 @@ class NPC {
     /*
      * Getters
      */
-     public String getName() {
-      return this.name;
-     }
+    public String getName() {
+        return this.name;
+    }
 
-     public String getLocation() {
-      if (this.location == (0000)) {
-          return "Tavern";
-      } else if (this.location == (1000)) {
-          return "Forest";
-      } else if (this.location == (0100)) {
-          return "Ocean";
-      } else if (this.location == (0010)) {
-          return "Village";
-      } else {
-          return "Kingdom";
-      }
-  }
+    public String getLocation() {
+        if (this.location == 0000) {
+            return "Tavern";
+        } else if (this.location == 1000) {
+            return "Forest";
+        } else if (this.location == 0100) {
+            return "Ocean";
+        } else if (this.location == 0010) {
+            return "Village";
+        } else {
+            return "Kingdom";
+        }
+    }
 
     public int getArmor() {
       return this.npcArmour;
@@ -62,64 +59,114 @@ class NPC {
     }
 
     public int getSkill() {
-      return this.skill;
+      return this.sk
     }
     /**
      * @param userChoice is the input from the player 
      * Splits the player's input up into a list of words and checks those to see if there is a mention of an NPC. Checks to see if the player is in the same location as the NPC, and if they are, prints out the NPC's default dialogue.
      */
     public static void talkToUser(String userChoice, Character Player) throws FileNotFoundException {
-      for (NPC NPC : Game.NPCs) {
-        String[] words = userChoice.split("\\s+");
-        for (String word : words) {
-          if (word.equalsIgnoreCase(NPC.getName())) {
-            if (Player.getLocation().equals(NPC.getLocation())) {
-              File npcDialogue = new File("Dialogue.txt");
-              Scanner fileReader = new Scanner(npcDialogue);
-              while (fileReader.hasNextLine()) {
-                  String data = fileReader.nextLine();
-                  if (data.contains(NPC.getName()) && data.contains("default:")) {
-                    System.out.println(NPC.getName() + ": " + data.substring(data.indexOf("default: ") + "default: ".length()));
-                  }
-              }
-            fileReader.close();
-            return; 
+        for (NPC NPC : Game.NPCs) {
+            String[] words = userChoice.split("\\s+");
+            for (String word : words) {
+                if (word.equalsIgnoreCase(NPC.getName())) {
+                    if (Player.getLocation().equals(NPC.getLocation())) {
+                        File npcDialogue = new File("Dialogue.txt");
+                        Scanner fileReader = new Scanner(npcDialogue);
+                        while (fileReader.hasNextLine()) {
+                            String data = fileReader.nextLine();
+                            if (data.contains(NPC.getName()) && data.contains("default:")) {
+                                System.out.println(NPC.getName() + ": " + data.substring(data.indexOf("default: ") + "default: ".length()));
+                            }
+                        }
+                        fileReader.close();
+                        return; 
+                    }
+                }
             }
-          }
         }
-      }
-    System.out.println("That character does not exist or is not in the same location as you are!");
+        System.out.println("That character does not exist or is not in the same location as you are!");
+    }
+
+    /**
+     * @param userChoice is the input from the player 
+     * Splits the player's input up into a list of words and checks those to see if there is a mention of an NPC. Checks to see if the player is in the same location as the NPC, and if they are, prints out the NPC's fight dialogue.
+     */
+    public static void fightDialogue(String userChoice, Character Player) throws FileNotFoundException {
+        for (NPC NPC : Game.NPCs) {
+            String[] words = userChoice.split("\\s+");
+            for (String word : words) {
+                if (word.equalsIgnoreCase(NPC.getName())) {
+                    if (Player.getLocation().equals(NPC.getLocation())) {
+                        File npcDialogue = new File("Dialogue.txt");
+                        Scanner fileReader = new Scanner(npcDialogue);
+                        while (fileReader.hasNextLine()) {
+                            String data = fileReader.nextLine();
+                            if (data.contains(NPC.getName()) && data.contains("fight:")) {
+                                System.out.println(NPC.getName() + ": " + data.substring(data.indexOf("fight: ") + "fight: ".length()));
+                            }
+                        }
+                        fileReader.close();
+                        return; 
+                    }
+                }
+            }
+        }
+        System.out.println("That character does not exist or is not in the same location as you are!");
+    }
+
+    /**
+     * @param userChoice is the input from the player 
+     * Splits the player's input up into a list of words and checks those to see if there is a mention of an NPC. Checks to see if the player is in the same location as the NPC, and if they are, prints out the NPC's trade dialogue.
+     */
+    public static void tradeDialogue(String userChoice, Character Player) throws FileNotFoundException {
+        for (NPC NPC : Game.NPCs) {
+            String[] words = userChoice.split("\\s+");
+            for (String word : words) {
+                if (word.equalsIgnoreCase(NPC.getName())) {
+                    if (Player.getLocation().equals(NPC.getLocation())) {
+                        File npcDialogue = new File("Dialogue.txt");
+                        Scanner fileReader = new Scanner(npcDialogue);
+                        while (fileReader.hasNextLine()) {
+                            String data = fileReader.nextLine();
+                            if (data.contains(NPC.getName()) && data.contains("trade:")) {
+                                System.out.println(NPC.getName() + ": " + data.substring(data.indexOf("trade: ") + "trade: ".length()));
+                            }
+                        }
+                        fileReader.close();
+                        return; 
+                    }
+                }
+            }
+        }
+        System.out.println("That character does not exist or is not in the same location as you are!");
     }
 
     public static String tradeObject() {
-        private Hashtable <String, Boolean> collection; 
-  //collection creates a hashtable to store items in inventory as well as whether or not they are already in inventory
+        Hashtable<String, Boolean> collection; 
+        return "";
     }
-  public static String talkToUser(){
-    System.out.println("Hi! My name is" + name + ". What's yours?");
-  //  if (userInput == ){
-   // }
-  }
-  public static String tradeObject(){
-    //restock, inventory, new arrayList for this class
-    // inventory stores certain amount of things 
-  }
-  public void addItem(String item){
-    this.inventory.put(item, true);
 
-  }
-  public String removeItem(String item){
-    this.inventory.remove(item);
-    return item;
-  } // return the item that we removed
+    //collection creates a hashtable to store items in inventory as well as whether or not they are already in inventory
+
+    // inventory stores certain amount of things 
+    public void addItem(String item){
+        this.inventory.put(item, true);
+    }
+
+    public String removeItem(String item){
+        this.inventory.remove(item);
+        return item;
+    } // return the item that we removed
 
     public boolean containsItem(String item){
-      return this.inventory.containsKey(item);
+        return this.inventory.containsKey(item);
     }
-      // returns true if the title appears as a key in the Libary's collection, false otherwise
+
+    // returns true if the title appears as a key in the Libary's collection, false otherwise
     public boolean isAvailable(String item){
-      return this.inventory.get(item);
+        return this.inventory.get(item);
     } // returns true if the item is currently in inventory, false otherwise
-  
-      return "";
 }
+
+
